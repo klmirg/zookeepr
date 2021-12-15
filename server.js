36f65a949d7ff1b511db62ae7a6f6848abd6dd1b
Ronya <klmirg6@gmail.com>
2021-12-14 23:14:34 -0600

    cleaned up a bit

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-// const fs = require('fs');
-// const path = require('path');
 const express = require('express');
-// const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
@@ -9,11 +7,11 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 
 
-app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 // Use apiRoutes
 app.use('/api', apiRoutes);
